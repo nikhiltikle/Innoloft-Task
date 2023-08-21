dfe8c734d5c38798d3e76fe59b28bb775fd8e566
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-08-21 15:53:15 +0530

    Add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
     <div />
-  </React.StrictMode>
+  </React.StrictMode>,
 );

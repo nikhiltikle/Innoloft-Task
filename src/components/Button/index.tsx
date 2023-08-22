69b1784b3bbd2b264060860d1804bc5b1bcc1740
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-08-22 21:40:13 +0530

    Add button style by variant

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,25 @@
 import { MouseEvent } from 'react';
 
 interface ButtonProps {
-  label: string;
+  label: string | React.ReactNode;
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
-  variant?: 'primary';
+  variant?: 'primary' | 'secondary';
+  type?: 'submit';
 }
 
-export default function Button({ label, onClick, variant = 'primary' }: ButtonProps) {
+export default function Button({ label, onClick, type, variant = 'primary', ...props }: ButtonProps) {
   const className = {
     primary: 'bg-chinese-blue border border-chinese-blue text-white text-sm text-regular',
+    secondary: 'bg-platinum border border-ghost-white text-sm text-regular text-chinese-blue',
   };
 
   return (
-    <button onClick={onClick} className={`px-2.5 py-1 rounded-md hover:opacity-80 ${className[variant]}`}>
+    <button
+      onClick={onClick}
+      type={type}
+      className={`px-2.5 py-1 rounded-md hover:opacity-80 ${className[variant]}`}
+      {...props}
+    >
       {label}
     </button>
   );

f47c7fd9675e246a15bb082e48f0267582900e5e
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-08-22 21:40:54 +0530

    Update CkEditor

--- src/components/CkEditor/index.tsx
+++ src/components/CkEditor/index.tsx
@@ -3,24 +3,19 @@ import './styles.css';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-export default function CkEditor() {
+interface CkEditorProps {
+  data: string;
+  onChange: (data: string) => void;
+}
+
+export default function CkEditor({ data, onChange }: CkEditorProps) {
   return (
     <CKEditor
       editor={ClassicEditor}
-      data="<p>Hello from CKEditor&nbsp;5!</p>"
-      onReady={editor => {
-        // You can store the "editor" and use when it is needed.
-        console.log('Editor is ready to use!', editor);
-      }}
+      data={data}
       onChange={(event, editor) => {
         const data = editor.getData();
-        console.log({ event, editor, data });
-      }}
-      onBlur={(event, editor) => {
-        console.log('Blur.', editor);
-      }}
-      onFocus={(event, editor) => {
-        console.log('Focus.', editor);
+        onChange(data);
       }}
     />
   );

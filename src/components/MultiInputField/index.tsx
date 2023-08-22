646e5c46a647e2c835bb3414c59a173923a34c53
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-08-22 21:41:10 +0530

    Add multi input field component

---
+++ src/components/MultiInputField/index.tsx
@@ -0,0 +1,46 @@
+import { Product } from 'common/interfaces/product';
+import Button from 'components/Button';
+import Input from 'components/Input';
+import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
+
+interface MultiInputFieldProps<T extends FieldValues> {
+  fields: { id: number; name: string }[];
+  onAddField: () => void;
+  onRemoveField: (index: number) => void;
+  name: keyof Product;
+  register: UseFormRegister<T>;
+  label: string;
+  icon: React.ReactNode;
+}
+
+export default function MultiInputField<T extends FieldValues>({
+  name,
+  label,
+  icon,
+  fields,
+  onAddField,
+  onRemoveField,
+  register,
+}: MultiInputFieldProps<T>) {
+  return (
+    <div className="flex gap-2 text-cadet-grey items-start flex-grow">
+      {icon}
+
+      <div className="flex flex-col gap-2.5 flex-grow">
+        <div className="flex gap-2.5 justify-between mb-2.5 items-start flex-grow">
+          <p className="text-base text-auro-metal-saurus">{label}</p>
+
+          <Button label="Add" onClick={onAddField} />
+        </div>
+
+        {fields.map((field, index) => (
+          <div key={`${index}-${field}`} className="flex gap-4 justify-between">
+            <Input name={`${name}.${index}.name` as Path<T>} register={register} required />
+
+            <Button label="Remove" variant="secondary" onClick={() => onRemoveField(index)} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}

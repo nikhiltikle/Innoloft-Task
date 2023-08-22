4967d96dd99f15fa458d664c5e80bf2533467ed7
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-08-22 21:40:38 +0530

    Update input to handle form state

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,20 +1,22 @@
-import { ChangeEvent } from 'react';
+import { FieldValues, Path, UseFormRegister } from 'react-hook-form';
 
-interface InputProps {
-  name: string;
+interface InputProps<T extends FieldValues> {
+  name: Path<T>;
   placeholder?: string;
-  value?: string;
-  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+  register: UseFormRegister<T>;
+  required?: boolean;
+  error?: boolean;
 }
 
-export default function Input({ name, onChange, placeholder, value }: InputProps) {
+export default function Input<T extends FieldValues>({ name, placeholder, register, required, error }: InputProps<T>) {
   return (
-    <input
-      name={name}
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-      className="rounded-md border-light-silver border px-2.5 py-1 text-charcoal text-base font-semibold placeholder:text-auro-metal-saurus placeholder:text-sm placeholder:font-normal w-full outline-0"
-    />
+    <>
+      <input
+        placeholder={placeholder}
+        className="rounded-md border-light-silver border px-2.5 py-1 text-charcoal text-base font-semibold placeholder:text-auro-metal-saurus placeholder:text-sm placeholder:font-normal w-full outline-0"
+        {...register(name, { required })}
+      />
+      {error && <span>Required field</span>}
+    </>
   );
 }

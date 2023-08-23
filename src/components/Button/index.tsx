40ef6c1aa89167c8e481765a3c1bf01b4baae369
Nikhil Tikle <nikhiltikle31@gmail.com>
2023-08-23 10:40:47 +0530

    Add support for icon in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,13 +1,14 @@
 import { MouseEvent } from 'react';
 
 interface ButtonProps {
-  label: string | React.ReactNode;
+  label: string;
   onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
   variant?: 'primary' | 'secondary' | 'text';
   type?: 'submit';
+  icon?: React.ReactNode;
 }
 
-export default function Button({ label, onClick, type, variant = 'primary', ...props }: ButtonProps) {
+export default function Button({ label, onClick, type, icon, variant = 'primary', ...props }: ButtonProps) {
   const className = {
     primary: 'bg-chinese-blue border border-chinese-blue text-white text-sm text-normal',
     secondary: 'bg-platinum border border-ghost-white text-sm text-normal text-chinese-blue',
@@ -18,9 +19,11 @@ export default function Button({ label, onClick, type, variant = 'primary', ...p
     <button
       onClick={onClick}
       type={type}
-      className={`px-2.5 py-1 rounded-md hover:opacity-80 ${className[variant]}`}
+      className={`px-2.5 py-1 rounded-md hover:opacity-80 flex items-center ${className[variant]}`}
       {...props}
     >
+      {icon && <span className="mr-1.5 inline-block">{icon}</span>}
+
       {label}
     </button>
   );
